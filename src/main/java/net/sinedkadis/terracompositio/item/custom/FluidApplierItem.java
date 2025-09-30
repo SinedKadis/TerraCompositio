62230f1d721a34daf8f4fdf447b6ac50b0a18606
SinedKadis <sidbov@gmail.com>
2025-09-30 22:59:30 +0300

    Fluid Applier WIP 2

--- src/main/java/net/sinedkadis/terracompositio/item/custom/FluidApplierItem.java
+++ src/main/java/net/sinedkadis/terracompositio/item/custom/FluidApplierItem.java
@@ -6,6 +6,7 @@ import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvent;
@@ -34,12 +35,13 @@ import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.common.SoundActions;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.event.ForgeEventFactory;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidUtil;
-import net.minecraftforge.fluids.IFluidTank;
+import net.minecraftforge.fluids.capability.IFluidHandler;
 import net.minecraftforge.fluids.capability.IFluidHandlerItem;
-import net.minecraftforge.fluids.capability.templates.FluidTank;
+import net.minecraftforge.fluids.capability.wrappers.FluidBucketWrapper;
 
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -49,11 +51,9 @@ import java.util.Optional;
 @SuppressWarnings("deprecation")
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
-public class FluidApplierItem extends Item implements IFluidHandlerItem, DispensibleContainerItem {
-    private final IFluidTank tank = new FluidTank(8000);
-
+public class FluidApplierItem extends Item implements DispensibleContainerItem {
     public FluidApplierItem(Properties pProperties) {
-        super(pProperties);
+        super(pProperties.durability(8));
     }
 
     public static int getRenderAmount(ItemStack stack) {
@@ -70,58 +70,73 @@ public class FluidApplierItem extends Item implements IFluidHandlerItem, Dispens
 
     public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pHand) {
         ItemStack itemstack = pPlayer.getItemInHand(pHand);
-        IFluidHandlerItem fluidHandlerItem = (IFluidHandlerItem) itemstack.getItem();
-        Fluid fluid = fluidHandlerItem.getFluidInTank(0).getFluid();
-        BlockHitResult blockhitresult = getPlayerPOVHitResult(pLevel, pPlayer, fluid == Fluids.EMPTY ? ClipContext.Fluid.SOURCE_ONLY : ClipContext.Fluid.NONE);
-        InteractionResultHolder<ItemStack> ret = ForgeEventFactory.onBucketUse(pPlayer, pLevel, itemstack, blockhitresult);
-        if (ret != null) return ret;
-        if (blockhitresult.getType() == HitResult.Type.MISS) {
-            return InteractionResultHolder.pass(itemstack);
-        } else if (blockhitresult.getType() != HitResult.Type.BLOCK) {
-            return InteractionResultHolder.pass(itemstack);
-        } else {
-            BlockPos blockpos = blockhitresult.getBlockPos();
-            Direction direction = blockhitresult.getDirection();
-            BlockPos blockpos1 = blockpos.relative(direction);
-            if (pLevel.mayInteract(pPlayer, blockpos) && pPlayer.mayUseItemAt(blockpos1, direction, itemstack)) {
-                if (fluid == Fluids.EMPTY) {
-                    BlockState blockstate1 = pLevel.getBlockState(blockpos);
-                    if (blockstate1.getBlock() instanceof BucketPickup bucketpickup) {
-                        Fluid fluid1 = ((BucketItem) bucketpickup.pickupBlock(pLevel, blockpos, blockstate1)
-                                .getItem()).getFluid();
-                        if (fluidHandlerItem.fill(new FluidStack(fluid1,1000),FluidAction.SIMULATE) == 1000) {
-                            pPlayer.awardStat(Stats.ITEM_USED.get(this));
-                            bucketpickup.getPickupSound(blockstate1).ifPresent((p_150709_) -> pPlayer.playSound(p_150709_, 1.0F, 1.0F));
-                            pLevel.gameEvent(pPlayer, GameEvent.FLUID_PICKUP, blockpos);
-                            if (!pLevel.isClientSide) {
-                                CriteriaTriggers.FILLED_BUCKET.trigger((ServerPlayer)pPlayer, itemstack);
-                            }
-                            fluidHandlerItem.fill(new FluidStack(fluid1,1000),FluidAction.EXECUTE);
-                            return InteractionResultHolder.sidedSuccess(itemstack, pLevel.isClientSide());
-                        }
-                    }
-
-                    return InteractionResultHolder.fail(itemstack);
-                } else {
-                    BlockState blockstate = pLevel.getBlockState(blockpos);
-                    BlockPos blockpos2 = canBlockContainFluid(pLevel, blockpos, blockstate, itemstack) ? blockpos : blockpos1;
-                    if (this.emptyContents(pPlayer, pLevel, blockpos2, blockhitresult, itemstack)) {
-                        this.checkExtraContent(pPlayer, pLevel, itemstack, blockpos2);
-                        if (pPlayer instanceof ServerPlayer) {
-                            CriteriaTriggers.PLACED_BLOCK.trigger((ServerPlayer)pPlayer, blockpos2, itemstack);
-                        }
-
-                        pPlayer.awardStat(Stats.ITEM_USED.get(this));
-                        return InteractionResultHolder.sidedSuccess(itemstack, pLevel.isClientSide());
-                    } else {
-                        return InteractionResultHolder.fail(itemstack);
-                    }
-                }
+        Optional<IFluidHandlerItem> fluidHandler = FluidUtil.getFluidHandler(itemstack).resolve();
+        if (fluidHandler.isPresent()){
+            IFluidHandlerItem fluidHandlerItem = fluidHandler.get();
+            Fluid fluid = fluidHandlerItem.getFluidInTank(0).getFluid();
+            BlockHitResult blockhitresult = getPlayerPOVHitResult(pLevel, pPlayer, fluid == Fluids.EMPTY ? ClipContext.Fluid.SOURCE_ONLY : ClipContext.Fluid.ANY);
+            InteractionResultHolder<ItemStack> ret = ForgeEventFactory.onBucketUse(pPlayer, pLevel, itemstack, blockhitresult);
+            if (ret != null) return ret;
+            if (blockhitresult.getType() == HitResult.Type.MISS) {
+                return InteractionResultHolder.pass(itemstack);
+            } else if (blockhitresult.getType() != HitResult.Type.BLOCK) {
+                return InteractionResultHolder.pass(itemstack);
             } else {
-                return InteractionResultHolder.fail(itemstack);
+                BlockPos blockpos = blockhitresult.getBlockPos();
+                Direction direction = blockhitresult.getDirection();
+                BlockPos blockpos1 = blockpos.relative(direction);
+                if (pLevel.mayInteract(pPlayer, blockpos) && pPlayer.mayUseItemAt(blockpos1, direction, itemstack)) {
+                    if (fluid == Fluids.EMPTY) {
+                        BlockState blockstate1 = pLevel.getBlockState(blockpos);
+                        if (blockstate1.getBlock() instanceof BucketPickup bucketpickup) {
+                            Fluid fluid1 = ((BucketItem) bucketpickup.pickupBlock(pLevel, blockpos, blockstate1)
+                                    .getItem()).getFluid();
+                            if (fluidHandlerItem.fill(new FluidStack(fluid1, 1000), IFluidHandler.FluidAction.SIMULATE) == 1000) {
+                                pPlayer.awardStat(Stats.ITEM_USED.get(this));
+                                bucketpickup.getPickupSound(blockstate1).ifPresent((p_150709_) -> pPlayer.playSound(p_150709_, 1.0F, 1.0F));
+                                pLevel.gameEvent(pPlayer, GameEvent.FLUID_PICKUP, blockpos);
+                                if (!pLevel.isClientSide) {
+                                    CriteriaTriggers.FILLED_BUCKET.trigger((ServerPlayer) pPlayer, itemstack);
+                                    fluidHandlerItem.fill(new FluidStack(fluid1, 1000), IFluidHandler.FluidAction.EXECUTE);
+                                }
+                                return InteractionResultHolder.sidedSuccess(itemstack, pLevel.isClientSide);
+                            }
+                        }
+                        return InteractionResultHolder.fail(itemstack);
+                    } else {
+                        BlockState blockstate = pLevel.getBlockState(blockpos);
+                        BlockPos blockpos2 = canBlockContainFluid(pLevel, blockpos, blockstate, itemstack) ? blockpos : blockpos1;
+                        if (this.emptyContents(pPlayer, pLevel, blockpos2, blockhitresult, itemstack)) {
+                            this.checkExtraContent(pPlayer, pLevel, itemstack, blockpos2);
+                            if (pPlayer instanceof ServerPlayer) {
+                                CriteriaTriggers.PLACED_BLOCK.trigger((ServerPlayer) pPlayer, blockpos2, itemstack);
+                                fluidHandlerItem.drain(1000, IFluidHandler.FluidAction.EXECUTE);
+                            }
+
+                            pPlayer.awardStat(Stats.ITEM_USED.get(this));
+                            return InteractionResultHolder.sidedSuccess(itemstack, pLevel.isClientSide());
+                        } else {
+                            return InteractionResultHolder.fail(itemstack);
+                        }
+                    }
+                } else {
+                    return InteractionResultHolder.fail(itemstack);
+                }
             }
         }
+        return super.use(pLevel,pPlayer,pHand);
     }
+
+    @Override
+    public int getDamage(ItemStack stack) {
+        return getRenderAmount(stack);
+    }
+
+    @Override
+    public boolean isDamageable(ItemStack stack) {
+        return false;
+    }
+
 
     protected static BlockHitResult getPlayerPOVHitResult(Level pLevel, Player pPlayer, ClipContext.Fluid pFluidMode) {
         float f = pPlayer.getXRot();
@@ -135,20 +150,21 @@ public class FluidApplierItem extends Item implements IFluidHandlerItem, Dispens
         float f7 = f2 * f4;
         double d0 = pPlayer.getBlockReach()*5;
         Vec3 vec31 = vec3.add((double)f6 * d0, (double)f5 * d0, (double)f7 * d0);
-        return pLevel.clip(new ClipContext(vec3, vec31, ClipContext.Block.COLLIDER, pFluidMode, pPlayer));
+        return pLevel.clip(new ClipContext(vec3, vec31, ClipContext.Block.OUTLINE, pFluidMode, pPlayer));
     }
 
     public boolean emptyContents(@Nullable Player pPlayer, Level pLevel, BlockPos pPos, @Nullable BlockHitResult pResult, @Nullable ItemStack container) {
         Optional<ItemStack> itemStack = Optional.ofNullable(container);
-        Optional<FluidStack> containedFluidStack = itemStack.flatMap(FluidUtil::getFluidContained);
-        Fluid content = containedFluidStack.isPresent() ? containedFluidStack.get().getFluid() : Fluids.EMPTY;
         Optional<IFluidHandlerItem> handlerItem;
-        handlerItem = itemStack.map(stack -> (IFluidHandlerItem) stack.getItem());
+        handlerItem = itemStack
+                .map(stack -> FluidUtil.getFluidHandler(stack).resolve())
+                .filter(Optional::isPresent)
+                .map(Optional::get);
+        Optional<FluidStack> containedFluidStack = handlerItem.map(iFluidHandlerItem -> iFluidHandlerItem.getFluidInTank(0));
+        Fluid content = containedFluidStack.isPresent() ? containedFluidStack.get().getFluid() : Fluids.EMPTY;
         if (handlerItem.isPresent()
                 && handlerItem.get()
-                    .drain(1000, FluidAction.SIMULATE).getAmount() != 1000) return false;
-        Runnable drain = () -> handlerItem.ifPresent(iFluidHandlerItem
-                -> iFluidHandlerItem.drain(1000, FluidAction.EXECUTE));
+                    .drain(1000, IFluidHandler.FluidAction.SIMULATE).getAmount() != 1000) return false;
         if (!(content instanceof FlowingFluid)) {
             return false;
         } else {
@@ -161,7 +177,6 @@ public class FluidApplierItem extends Item implements IFluidHandlerItem, Dispens
                 return pResult != null && this.emptyContents(pPlayer, pLevel, pResult.getBlockPos().relative(pResult.getDirection()), null, container);
             } else if (containedFluidStack.isPresent() && content.getFluidType().isVaporizedOnPlacement(pLevel, pPos, containedFluidStack.get())) {
                 content.getFluidType().onVaporize(pPlayer, pLevel, pPos, containedFluidStack.get());
-                drain.run();
                 return true;
             } else if (pLevel.dimensionType().ultraWarm() && content.is(FluidTags.WATER)) {
                 int i = pPos.getX();
@@ -172,12 +187,10 @@ public class FluidApplierItem extends Item implements IFluidHandlerItem, Dispens
                 for(int l = 0; l < 8; ++l) {
                     pLevel.addParticle(ParticleTypes.LARGE_SMOKE, (double)i + Math.random(), (double)j + Math.random(), (double)k + Math.random(), 0.0D, 0.0D, 0.0D);
                 }
-                drain.run();
                 return true;
             } else if (block instanceof LiquidBlockContainer && ((LiquidBlockContainer)block).canPlaceLiquid(pLevel,pPos,blockstate, content)) {
                 ((LiquidBlockContainer)block).placeLiquid(pLevel, pPos, blockstate, ((FlowingFluid) content).getSource(false));
                 handlerItem.ifPresent(iFluidHandlerItem -> this.playEmptySound(pPlayer, pLevel, pPos, iFluidHandlerItem));
-                drain.run();
                 return true;
             } else {
                 if (!pLevel.isClientSide && canBeReplaced && !blockstate.liquid()) {
@@ -206,50 +219,13 @@ public class FluidApplierItem extends Item implements IFluidHandlerItem, Dispens
 
     @Override
     public void appendHoverText(ItemStack pStack, @org.jetbrains.annotations.Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
-        FluidStack fluidStack = ((IFluidHandlerItem) pStack.getItem()).getFluidInTank(0);
-        pTooltipComponents.add(Component.translatable(fluidStack.getTranslationKey()).withStyle(ChatFormatting.GRAY));
-        pTooltipComponents.add(Component.literal(fluidStack.getAmount()+"mB").withStyle(ChatFormatting.GRAY));
-        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
-    }
-
-    @Override
-    public ItemStack getContainer() {
-        return this.getDefaultInstance();
-    }
-
-    @Override
-    public int getTanks() {
-        return 1;
-    }
-
-    @Override
-    public FluidStack getFluidInTank(int tank) {
-        return this.tank.getFluid();
-    }
-
-    @Override
-    public int getTankCapacity(int tank) {
-        return tank == 0 ? this.tank.getCapacity() : 0;
-    }
-
-    @Override
-    public boolean isFluidValid(int tank, FluidStack stack) {
-        return tank == 0 && this.tank.isFluidValid(stack);
-    }
-
-    @Override
-    public int fill(FluidStack resource, FluidAction action) {
-        return this.tank.fill(resource,action);
-    }
-
-    @Override
-    public FluidStack drain(FluidStack resource, FluidAction action) {
-        return this.tank.drain(resource,action);
-    }
-
-    @Override
-    public FluidStack drain(int maxDrain, FluidAction action) {
-        return this.tank.drain(maxDrain,action);
+        Optional<IFluidHandlerItem> fluidHandler = FluidUtil.getFluidHandler(pStack).resolve();
+        if (fluidHandler.isPresent()) {
+            FluidStack fluidStack = fluidHandler.get().getFluidInTank(0);
+            pTooltipComponents.add(Component.translatable(fluidStack.getTranslationKey()).withStyle(ChatFormatting.GRAY));
+            pTooltipComponents.add(Component.literal(fluidStack.getAmount() + "mB").withStyle(ChatFormatting.GRAY));
+            super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
+        }
     }
 
     @Override
@@ -261,5 +237,78 @@ public class FluidApplierItem extends Item implements IFluidHandlerItem, Dispens
         return blockstate.getBlock() instanceof LiquidBlockContainer
                 && ((LiquidBlockContainer)blockstate.getBlock())
                 .canPlaceLiquid(worldIn, posIn, blockstate, ((IFluidHandlerItem) itemstack.getItem()).getFluidInTank(0).getFluid());
+    }
+
+    @Override
+    public @org.jetbrains.annotations.Nullable ICapabilityProvider initCapabilities(ItemStack stack, @org.jetbrains.annotations.Nullable CompoundTag nbt) {
+        return new TCFluidBucketWrapper(stack);
+    }
+
+    private static class TCFluidBucketWrapper extends FluidBucketWrapper {
+        public TCFluidBucketWrapper(ItemStack stack) {
+            super(stack);
+        }
+
+        @Override
+        public FluidStack getFluid() {
+            CompoundTag tag = container.getOrCreateTag();
+            return FluidStack.loadFluidStackFromNBT(tag);
+        }
+
+        @Override
+        protected void setFluid(FluidStack fluidStack) {
+            CompoundTag tag = container.getOrCreateTag();
+            fluidStack.writeToNBT(tag);
+        }
+
+        @Override
+        public int getTankCapacity(int tank) {
+            return 8000;
+        }
+
+        @Override
+        public int fill(FluidStack resource, FluidAction action) {
+            FluidStack current = getFluid();
+            FluidStack toAdd = FluidStack.EMPTY;
+            if (resource.isFluidEqual(current) || current.isEmpty()) {
+                toAdd = resource.copy();
+                toAdd.setAmount(Math.min(getTankCapacity(0) - current.getAmount(),resource.getAmount()));
+                if (action.execute()){
+                    current = toAdd.copy();
+                    current.setAmount(current.getAmount() + toAdd.getAmount());
+                    setFluid(current);
+                }
+            }
+            return toAdd.getAmount();
+        }
+
+        @Override
+        public FluidStack drain(int maxDrain, FluidAction action) {
+            FluidStack current = getFluid();
+            if (current.isEmpty()) return current;
+            FluidStack toDrain = current.copy();
+            toDrain.setAmount(Math.max(current.getAmount(),maxDrain));
+            if (action.execute()){
+                current.setAmount(current.getAmount() - toDrain.getAmount());
+                setFluid(current);
+            }
+            return toDrain;
+        }
+
+        @Override
+        public FluidStack drain(FluidStack resource, FluidAction action) {
+            FluidStack current = getFluid();
+            if (resource.isFluidEqual(current)) {
+                int maxDrain = resource.getAmount();
+                FluidStack toDrain = current.copy();
+                toDrain.setAmount(Math.max(current.getAmount(), maxDrain));
+                if (action.execute()) {
+                    current.setAmount(current.getAmount() - toDrain.getAmount());
+                    setFluid(current);
+                }
+                return toDrain;
+            }
+            return FluidStack.EMPTY;
+        }
     }
 }
