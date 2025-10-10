1a037d0e1bf56ed3994a8d39259fb2bb67751654
SinedKadis <sidbov@gmail.com>
2025-10-10 14:06:47 +0300

    ItemEntity Bucket WIP

--- src/main/java/net/sinedkadis/terracompositio/mixin/ItemEntityRendererMixin.java
+++ src/main/java/net/sinedkadis/terracompositio/mixin/ItemEntityRendererMixin.java
@@ -2,9 +2,8 @@ package net.sinedkadis.terracompositio.mixin;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.entity.ItemRenderer;
-import net.minecraft.client.resources.model.BakedModel;
-import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.client.renderer.entity.ItemEntityRenderer;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidUtil;
@@ -18,24 +17,27 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Optional;
 
-@Mixin(ItemRenderer.class)
-public abstract class ItemRendererMixin {
+@Mixin(ItemEntityRenderer.class)
+public abstract class ItemEntityRendererMixin {
 
     @Shadow
-    public abstract void render(ItemStack pItemStack, ItemDisplayContext pDisplayContext, boolean pLeftHand, PoseStack pPoseStack, MultiBufferSource pBuffer, int pCombinedLight, int pCombinedOverlay, BakedModel pModel);
+    public abstract void render(ItemEntity pEntity, float pEntityYaw, float pPartialTicks, PoseStack pPoseStack, MultiBufferSource pBuffer, int pPackedLight);
 
     @Inject(
-            method = "render(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemDisplayContext;ZLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;IILnet/minecraft/client/resources/model/BakedModel;)V",
+            method = "render(Lnet/minecraft/world/entity/item/ItemEntity;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V",
             at = @At("HEAD")
     )
-    private void beforeRenderItem(ItemStack pItemStack, ItemDisplayContext pDisplayContext, boolean pLeftHand, PoseStack pPoseStack, MultiBufferSource pBuffer, int pCombinedLight, int pCombinedOverlay, BakedModel pModel, CallbackInfo ci) {
+    private void beforeRenderItem(ItemEntity pEntity, float pEntityYaw, float pPartialTicks, PoseStack pPoseStack, MultiBufferSource pBuffer, int pPackedLight, CallbackInfo ci) {
+        ItemStack pItemStack = pEntity.getItem();
         if (pItemStack.is(TCItems.FLUID_APPLIER.get())) {
             Optional<IFluidHandlerItem> fluidHandler = FluidUtil.getFluidHandler(pItemStack).resolve();
             if (fluidHandler.isPresent()) {
                 FluidStack fluidStack = fluidHandler.get().getFluidInTank(0);
                 if (fluidStack.isEmpty()) return;
                 ItemStack toRender = fluidStack.getFluid().getBucket().getDefaultInstance();
-                ((ItemRenderer)(Object)this).render(toRender,pDisplayContext,pLeftHand,pPoseStack,pBuffer,pCombinedLight,pCombinedOverlay,pModel);
+                ItemEntity pEntity1 = new ItemEntity(pEntity);
+                pEntity1.setItem(toRender);
+                this.render(pEntity1,pEntityYaw,pPartialTicks,pPoseStack,pBuffer,pPackedLight);
             }
         }
     }
