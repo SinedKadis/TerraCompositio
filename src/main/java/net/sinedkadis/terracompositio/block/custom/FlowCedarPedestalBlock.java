0875790ac0aeddb9eda14cadaedd36838ef69c4c
SinedKadis <sidbov@gmail.com>
2026-05-09 15:54:40 +0300

    Fixed cedar tank pedestal creation

--- src/main/java/net/sinedkadis/terracompositio/block/custom/FlowCedarPedestalBlock.java
+++ src/main/java/net/sinedkadis/terracompositio/block/custom/FlowCedarPedestalBlock.java
@@ -1,30 +1,36 @@
 package net.sinedkadis.terracompositio.block.custom;
 
+import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.IPlantable;
 import net.minecraftforge.common.PlantType;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
 public class FlowCedarPedestalBlock extends Block implements IPlantable {
     public FlowCedarPedestalBlock(Properties pProperties) {
         super(pProperties);
     }
 
-    public boolean canSurvive(BlockState pState, @NotNull LevelReader pLevel, BlockPos pPos) {
+    @SuppressWarnings("deprecation")
+    public boolean canSurvive(BlockState pState, LevelReader pLevel, BlockPos pPos) {
         BlockPos blockpos = pPos.below();
         return pState.getBlock() == this ? pLevel.getBlockState(blockpos).canSustainPlant(pLevel, blockpos, Direction.UP, this) : pState.is(BlockTags.DIRT) || pState.is(Blocks.FARMLAND);
     }
 
     @Override
-    public boolean propagatesSkylightDown(@NotNull BlockState state, @NotNull BlockGetter reader, @NotNull BlockPos pos) {
+    public boolean propagatesSkylightDown(BlockState state, BlockGetter reader, BlockPos pos) {
         return true;
     }
 
@@ -42,5 +48,15 @@ public class FlowCedarPedestalBlock extends Block implements IPlantable {
     @Override
     public boolean canConnectRedstone(BlockState state, BlockGetter level, BlockPos pos, @Nullable Direction direction) {
         return true;
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public void onRemove(BlockState pState, Level pLevel, BlockPos pPos, BlockState pNewState, boolean pMovedByPiston) {
+        super.onRemove(pState, pLevel, pPos, pNewState, pMovedByPiston);
+        BlockState blockState = pLevel.getBlockState(pPos.above());
+        if (!pState.equals(pNewState) && blockState.hasProperty(FlowCedarTankBlock.STAGE)) {
+            pLevel.setBlockAndUpdate(pPos.above(), blockState.setValue(FlowCedarTankBlock.STAGE, 3));
+        }
     }
 }
